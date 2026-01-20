35debf7f62da52fb6abaf7ad31040d6a1f59a7da
umilosev <umilosev12822rn@raf.rs>
2026-01-20 19:37:33 +0100

    Add Form Control to the add-post form

--- src/components/add-post-dialog/add-post-dialog.ts
+++ src/components/add-post-dialog/add-post-dialog.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule , Validators} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -14,6 +14,11 @@ import { Post } from '../../models/post';
 import { PostService } from '../../services/post-service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ReactiveFormsModule } from '@angular/forms';
+import { merge } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { signal } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-add-post-dialog',
@@ -32,16 +37,36 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './add-post-dialog.css',
 })
 export class AddPostDialog {
+  
   private dialogRef = inject(MatDialogRef<AddPostDialog>);
   private data = inject<Post | null>(MAT_DIALOG_DATA);
   private postService = inject(PostService);
   private snackBar = inject(MatSnackBar);
+  readonly email = new FormControl('',[Validators.required, Validators.email]);
+  readonly title = new FormControl('',[Validators.required, Validators.minLength(5)]);
+  readonly body = new FormControl('',[Validators.required, Validators.minLength(10)]);
 
+  titleErrorMessage = signal('');
+  bodyErrorMessage = signal('');
+  emailErrorMessage = signal('');
 
+  constructor() {
+    merge(this.email.statusChanges, 
+      this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateEmailMessage());
+    merge(this.title.statusChanges, 
+      this.title.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateTitleMessage());
+    merge(this.body.statusChanges, 
+      this.body.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateBodyMessage());
+  }
   post: Post = this.data
     ? { ...this.data }
     : { userId: 2, id: 101, title: '', email: '', body: '' };
-
   save(): void {
     this.postService.addPost(this.post).subscribe({
       next: (createdPost) => {
@@ -56,11 +81,38 @@ export class AddPostDialog {
       duration: 3000,
     });
   }
-
   cancel(): void {
     this.dialogRef.close();
     this.snackBar.open('Cancelled!', 'Close', {
       duration: 3000,
     });
+  }
+
+  updateEmailMessage() {
+    if (this.email.hasError('required')) {
+      this.emailErrorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.emailErrorMessage.set('Not a valid email');
+    } else {
+      this.emailErrorMessage.set('');
+    }
+  }
+  updateBodyMessage() {
+    if (this.body.hasError('required')) {
+      this.bodyErrorMessage.set('You must enter a value');
+    } else if (this.body.hasError('minlength')) {
+      this.bodyErrorMessage.set('Body must be at least 10 characters long');
+    } else {
+      this.bodyErrorMessage.set('');
+    }
+  }
+  updateTitleMessage() {
+    if (this.title.hasError('required')) {
+      this.titleErrorMessage.set('You must enter a value');
+    } else if (this.title.hasError('minlength')) {
+      this.titleErrorMessage.set('Title must be at least 5 characters long');
+    } else {
+      this.titleErrorMessage.set('');
+    }
   }
 }
